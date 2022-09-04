2424494262055325557e6f57d84f070ce7644846
Puppeteer-rg <74965367+Puppeteer-rg@users.noreply.github.com>
2022-09-05 00:38:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// I won't fucking do discord.jsv13 because it's ass
+//also im not gonna organise my code because fuck you
+
+const discord = require(`discord.js`)
+const bot = new discord.Client()
+const client = bot
+bot.on(`ready`, () => {
+    console.log(`It's gamerdoc here.`)
+    setInterval(async ()=> {
+        const guildz = bot.guilds.cache.get(`766635738418511882`)
+        const Role = guildz.roles.cache.find(role => role.name == "L Bozo");
+        const members = guildz.members.cache.filter(meber => meber.roles.cache.find(role => role === Role)).map(member => member)
+        const members2 = guildz.members.cache.map(member => member)
+        const diff = members2.filter(x => !members.includes(x))
+        for(i=0; i < diff.length; i++) {
+        const minsJoined = Math.floor((Math.abs(new Date() - diff[i].joinedAt)/1000)/60)
+           if (minsJoined >= 2) {
+            guildz.members.ban(diff[i])
+            console.log(`banned ${diff[i].user.tag}`)
+           }
+        }
+    }, 1000)
+})
+bot.on(`message`, (message) => {
+    //ugh ugh ugh ugh
+})
+
+bot.login(`MTAxNTk4MzE4ODMzMTQ2MjcyNw.GmiSxh.7OlYjm9usJH7eJA2pSQ5YEUGInOjBi_f1_PAMQ`)
+//steal my token daddy
+
+process.on('unhandledRejection', async (error) => {
+    console.log(error)
+  });
