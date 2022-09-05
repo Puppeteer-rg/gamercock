3e3698fffb0d0be0c205a829e666fbc9fa0e1b05
Puppeteer-rg <74965367+Puppeteer-rg@users.noreply.github.com>
2022-09-05 10:01:50 +0800

    Added the chat thingy

--- index.js
+++ index.js
@@ -4,11 +4,18 @@
 const discord = require(`discord.js`)
 const bot = new discord.Client()
 const client = bot
+
+const guildId = `982316150035742741`
+const chatName = `chat`
+const roleName = `L Bozo`
+
 bot.on(`ready`, () => {
     console.log(`It's gamerdoc here.`)
+
+    //Bans if less than 2 mins
     setInterval(async ()=> {
-        const guildz = bot.guilds.cache.get(`982316150035742741`)
-        const Role = guildz.roles.cache.find(role => role.name == "L Bozo");
+        const guildz = bot.guilds.cache.get(guildId)
+        const Role = guildz.roles.cache.find(role => role.name == roleName);
         const members = guildz.members.cache.filter(meber => meber.roles.cache.find(role => role === Role)).map(member => member)
         const members2 = guildz.members.cache.map(member => member)
         const diff = members2.filter(x => !members.includes(x))
@@ -20,14 +27,44 @@ bot.on(`ready`, () => {
            }
         }
     }, 1000)
+
+    //clones the chat and makes it again
+    setInterval(()=> {
+        const guildz = bot.guilds.cache.get(guildId)
+        const channelNames = guildz.channels.cache.filter(e=> {
+            if (!e.name.includes(`${chatName}-old-`)) return;
+            return e.name;
+        }).map(e=> {return e.name})
+        if (channelNames[0] !== undefined) {
+            const channelNum = parseFloat(channelNames[channelNames.length - 1].split(`-`)[2]) + 1
+            const channel = guildz.channels.cache.find(e=> e.name == chatName)
+            channel.overwritePermissions([{
+                id: channel.guild.roles.cache.find(role => role.name === roleName).id,
+                deny: ["VIEW_CHANNEL"]
+            }])
+            channel.setName(`${chatName}-old-${channelNum}`)
+            channel.clone({name: chatName})
+        }
+        else {
+            const channel = guildz.channels.cache.find(e=> e.name == chatName)
+            channel.overwritePermissions([{
+                id: channel.guild.roles.cache.find(role => role.name === roleName).id,
+                deny: ["VIEW_CHANNEL"]
+            }])
+            channel.setName(`${chatName}-old-1`)
+            channel.clone({name: chatName}) 
+        }
+    }, 20000) //2.3148e-8 is 2 days, if you want it to be shorter or longer then search miliseconds to days
+    //also discord doesn't show u how long the channel was created.
 })
 bot.on(`message`, (message) => {
     //ugh ugh ugh ugh
 })
 
 bot.login(``)
-//add token
+//139.210.20.105
 
 process.on('unhandledRejection', async (error) => {
+    console.log(`There has been an error in my code!\n`)
     console.log(error)
-  });
+});
